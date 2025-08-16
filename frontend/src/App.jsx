ffd7c6d949c930f689d1d4c6cd3de2f23f7ba805
kiran <n210714@rguktn.ac.in>
2025-08-16 14:02:20 +0530

    frontend created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='object-contsainer mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+      frontend file
+      
+    </div>
+  )
+}
+
+export default App
